4404b29660aec2920d352a86fcd2155a0510ad7b
Nicolas Bru Frantzen <nicolas.bru.frantzen@gmail.com>
2022-02-12 16:52:13 +0100

    Challenge 3.

--- src/challenges/set1.rs
+++ src/challenges/set1.rs
@@ -28,6 +28,7 @@ mod challenge1
     }
 }
 
+
 /// Fixed XOR
 mod challenge2
 {
@@ -56,6 +57,56 @@ mod challenge2
             let right = "686974207468652062756c6c277320657965";
 
             assert_eq!(xor_hex_str(left, right).unwrap(), "746865206b696420646f6e277420706c6179");
+        }
+    }
+}
+
+
+mod challenge3
+{
+    use anyhow::Result;
+
+    fn decipher(cipher: &str, key: &char) -> Result<String>
+    {
+        let mut key_arr = [0, 1];
+        key.encode_utf8(&mut key_arr);
+        let key = key_arr[0];
+
+        let iter: Vec<u8> = hex::decode(cipher)?
+            .iter()
+            .map(|c| c ^ key )
+            .collect();
+
+            Ok(String::from_utf8_lossy(&iter).into_owned())
+    }
+
+
+    #[cfg(test)]
+    mod tests
+    {
+        use super::*;
+
+        const SECRET: &str = "Cooking MC's like a pound of bacon";
+
+        fn break_cipher(cipher: &str) -> Option<char>
+        {
+            for c in 'A'..='Z'
+            {
+                match decipher(cipher, &c) {
+                    Ok(deciphered) if deciphered == SECRET => { return Some(c) },
+                    Ok(_) => (),
+                    Err(_) => (),
+                }
+            }
+
+            None
+        }
+
+        #[test]
+        fn test_challenge()
+        {
+            let cipher = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736";
+            assert_eq!(break_cipher(cipher), Some('X'));
         }
     }
 }
