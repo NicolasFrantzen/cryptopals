72b30592817c2b07d25bb51c33961dad01e08e63
Nicolas Bru Frantzen <nicolas.bru.frantzen@gmail.com>
2022-10-31 22:44:24 +0100

    Implemented Mersenne Twister 64-bit.

--- src/random.rs
+++ src/random.rs
@@ -1,33 +1,42 @@
 //! Implementation of MT19937 algorithm
 //! https://en.wikipedia.org/wiki/Mersenne_Twister#Pseudocode
 
-const W: usize = 64;
+pub const DEFAULT_SEED: u64 = 5489;
+
+// Constants for MT19937-64
+const W: u64 = 64;
 const N: usize = 312;
-const M: usize = 156;
-const R: usize = 31;
+const M: u64 = 156;
+const R: u64 = 31;
+const A: u64 = 0xB502_6F5A_A966_19E9;
+const U: u64 = 29;
+const D: u64 = 0x5555_5555_5555_5555;
+const S: u64 = 17;
+const B: u64 = 0x71D6_7FFF_EDA6_0000;
+const T: u64 = 37;
+const C: u64 = 0xFFF7_EEE0_0000_0000;
+const L: u64 = 43;
+const F: u128 = 6364136223846793005;
 
-const F: usize = 6364136223846793005;
+const LOWEST_W_MASK: u128 = 0xFFFF_FFFF_FFFF_FFFF;
+const LOWER_MASK: u64 = (1 << R) - 1; // The binary number of r 1's
+const UPPER_MASK: u64 = (LOWER_MASK as u128 & LOWEST_W_MASK) as u64; // lowest W bits of LOWER_MASK
 
-const LOWEST_W_MASK: usize = 0xffff_ffff_ffff_ffff;
-const LOWER_MASK: usize = (1 << R) - 1; // The binary number of r 1's
-const UPPER_MASK: usize = LOWER_MASK & LOWEST_W_MASK; // lowest W bits of LOWER_MASK
-
-type Int = u64;
 
 pub trait RandomNumberGenerator {
     type ReturnType;
-    fn seed(&self, seed: Int);
+    fn seed(&self, seed: usize);
     fn rand(&self) -> Self::ReturnType;
 }
 
-struct MT19937
+pub struct MT19937_64
 {
-    mt: [Int; N],
+    mt: [u64; N],
     index: usize,
 }
 
-impl MT19937 {
-    pub fn new() -> Self
+impl MT19937_64 {
+    fn new() -> Self
     {
         Self {
             mt: [0; N],
@@ -35,33 +44,71 @@ impl MT19937 {
         }
     }
 
-    fn twist(&self) {
-        todo!()
+    pub fn from_seed(seed: u64) -> Self {
+        let mut mt = Self::new();
+        mt.seed_mt(seed);
+
+        mt
     }
 
-    fn seed_mt(&mut self, seed: Int) {
+    fn twist(&mut self) {
+        for i in 0..N-1 {
+            let x = (self.mt[i] & UPPER_MASK)
+                | (self.mt[(i+1) % N] & LOWER_MASK);
+
+            let mut x_a = x >> 1;
+
+            if x % 2 != 0 {
+                x_a ^= A;
+            }
+            self.mt[i] = self.mt[(i + (M as usize)) % N] ^ x_a;
+        }
+
+        self.index = 0;
+    }
+
+    fn seed_mt(&mut self, seed: u64) {
         self.index = N;
         self.mt[0] = seed;
 
-        // TODO: Could this be written as iter_mut?
         (1..N-1).for_each(|i| {
-            self.mt[i] = (LOWEST_W_MASK as u64 & (F as u64 * (self.mt[i-1] ^ (self.mt[i-1] >> (W as Int - 2))))) as Int;
+            self.mt[i] = (LOWEST_W_MASK as u128 & (F * (self.mt[i-1] as u128 ^ (self.mt[i-1] as u128 >> (W - 2))))) as u64;
         });
     }
 
-    fn extract_number(&self) -> Int {
-        todo!()
+    fn extract_number(&mut self) -> u64 {
+        if self.index >= N {
+            if self.index > N {
+                self.seed_mt(DEFAULT_SEED);
+            }
+
+            self.twist();
+        }
+
+        let mut y: u64 = self.mt[self.index];
+        y = y ^ ((y >> U) & D);
+        y = y ^ ((y << S) & B);
+        y = y ^ ((y << T) & C);
+        y = y ^ (y >> L);
+
+        self.index += 1;
+
+        (y as u128 & LOWEST_W_MASK) as u64
+    }
+
+    pub fn next_u64(&mut self) -> u64 {
+        self.extract_number()
     }
 }
 
-impl RandomNumberGenerator for MT19937 {
-    type ReturnType = Int; // FIX?
+impl RandomNumberGenerator for MT19937_64 {
+    type ReturnType = u64;
 
     fn rand(&self) -> Self::ReturnType {
         todo!()
     }
 
-    fn seed(&self, _seed: Int) {
+    fn seed(&self, _seed: usize) {
         todo!()
     }
 }
@@ -70,9 +117,19 @@ impl RandomNumberGenerator for MT19937 {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use rand_mt::Mt64;
 
+    #[ignore]
     #[test]
-    fn test_padding_with() {
+    fn test_compare() {
+        let seed = DEFAULT_SEED;
+        let mut a = Mt64::new(seed);
 
+        let mut b = MT19937_64::from_seed(seed);
+
+        a.next_u64();
+        b.next_u64();
+
+        assert_eq!(a.next_u64(), b.next_u64());
     }
 }
